fceed952143465a412309b3140c2f6e51ca5b1e8
Juan Villela <juan@villela.co>
2023-03-08 16:37:54 -0500

    feat: Add sub menu UI and animations.

--- assets/scripts/header.ts
+++ assets/scripts/header.ts
@@ -1,6 +1,10 @@
 let mobileMenuOpen = false;
+let subMenuOpen = false;
+
 const mobileMenuBtn = document.querySelector('button#mobile-menu-toggle');
 const mobileMenu = document.querySelector('section#mobile-menu');
+const subMenuBtns = document.querySelectorAll('button#solutions-parent');
+const subMenus = document.querySelectorAll('ul#solutions-sub');
 
 mobileMenuBtn.addEventListener('click', () => {
   const openBtn = mobileMenuBtn?.querySelector('div#open');
@@ -12,4 +16,17 @@ mobileMenuBtn.addEventListener('click', () => {
   closeBtn?.setAttribute('data-active', !mobileMenuOpen);
 
   mobileMenuOpen = !mobileMenuOpen;
+});
+
+subMenuBtns.forEach((subMenuBtn) => {
+  subMenuBtn.addEventListener('click', () => {
+    subMenuBtn?.setAttribute('data-active', !subMenuOpen);
+
+    subMenus.forEach(subMenu => {
+      subMenu?.setAttribute('data-active', !subMenuOpen);
+      subMenu?.setAttribute('aria-hidden', subMenuOpen);
+    });
+
+    subMenuOpen = !subMenuOpen;
+  });
 });
