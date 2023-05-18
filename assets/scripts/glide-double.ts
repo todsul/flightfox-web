0b22a2e622eafdca4bca0706195c7b97a2be632f
Juan Villela <juan@villela.co>
2023-05-17 22:42:23 -0400

    fix: Add correct case studies glide controls.

--- assets/scripts/glide-double.ts
+++ assets/scripts/glide-double.ts
@@ -1,4 +1,14 @@
 import Glide, { Controls } from '@glidejs/glide/dist/glide.modular.esm';
 
 new Glide('.glide-1').mount({ Controls });
-new Glide('.glide-2', { gap: 32 }).mount({ Controls });
+new Glide('.glide-2', {
+  type: 'slider',
+  startAt: 0,
+  perView: 3,
+  gap: 32,
+  breakpoints: {
+    768: {
+      perView: 1,
+    },
+  },
+}).mount({ Controls });
